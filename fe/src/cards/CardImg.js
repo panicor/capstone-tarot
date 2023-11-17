bb297450049e65d933c3ecb33bf390d3aae032ab
Rachel Panico <rachelpanico@Rachels-Air.home>
2023-11-17 10:51:55 -0500

    image links

--- fe/src/cards/CardImg.js
+++ fe/src/cards/CardImg.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./CardImg.css";
 
-function CardImg({ name = "Ace of Cups", image="https://i.imgur.com/MC6A1jc.jpg" }) {
-console.log(image);
+function CardImg({ name = "The Fool", image="https://i.imgur.com/MC6A1jc.jpg" }) {
+  console.log(image);
   return <img
       className="Card"
       alt={name}
